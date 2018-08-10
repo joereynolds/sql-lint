9d31ac3547b3b767e366a72f1bfecec9f659c8b6
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2018-08-10 12:05:27 +0100

    Bump version in dist

--- dist/src/main.js
+++ dist/src/main.js
@@ -4,14 +4,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const program = require("commander");
 const process = require("process");
 const version = "0.0.2";
-function increaseVerbosity(v, total) {
-    return total + 1;
-}
 program
     .version(version)
     .option("-f, --file <path>", "The .sql file to lint")
     .option("-q, --query <string>", "The query to lint")
     .option("-v, --verbose", "Brings back information on the what it's linting and the tokens generated")
     .parse(process.argv);
+if (program.query) {
+    console.log("Linting query");
+}
+if (program.file) {
+    console.log("Linting file");
+}
 console.log("working");
 //# sourceMappingURL=main.js.map

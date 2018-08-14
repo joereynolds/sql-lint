1eab22f9c6586fc50f13fdc841e5912cbb3f4cb4
Joe Reynolds <joereynolds952@gmail.com>
2018-08-14 19:32:28 +0100

    Update integration-test.sh

--- integration-test.sh
+++ integration-test.sh
@@ -1,2 +1,7 @@
-docker pull mysql
-docker run --name sql-lint-mysql -e MYSQL_ROOT_PASSWORD=integration-tests -d mysql:latest
+docker pull mysql:5.6
+docker run -p=3306:3306 --name sql-lint-mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql:5.6
+
+sql-lint --host="localhost" --user="root" --password="integration-tests" --query="use te" 
+
+docker kill sql-lint-mysql
+docker rm sql-lint-mysql

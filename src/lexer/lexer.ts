02b10e60a88fee066541a2a5d5e4561f94f5675b
Joe Reynolds <joereynolds952@gmail.com>
2018-08-16 17:52:56 +0100

    Fix bug with queries being incorrectly categorised

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -1,48 +1,45 @@
 import { Query } from "../reader/reader";
 import { ILexer } from "./interface";
-import { Select } from "./select"
+import { Select } from "./select";
 import { Tokens } from "./tokens";
 import { Use } from "./use";
 
 function categorise(query: string) {
-    query = query.trim().toLowerCase();
+  query = query.trim().toLowerCase();
 
-    if (query.startsWith("select")) {
-        return "select";
-    }
+  if (query.startsWith("select")) {
+    return "select";
+  }
 
-    if (query.startsWith("delete")) {
-        return "delete";
-    }
+  if (query.startsWith("delete")) {
+    return "delete";
+  }
 
-    if (query.startsWith("update")) {
-        return "update";
-    }
+  if (query.startsWith("update")) {
+    return "update";
+  }
 
-    if (query.startsWith("use")) {
-        return "use";
-    }
+  if (query.startsWith("use")) {
+    return "use";
+  }
 
-    throw new Error(`Unable to categorise query: ${query}`);
+  throw new Error(`Unable to categorise query: ${query}`);
 }
 
 function tokenise(query: Query): Query {
-    const category = categorise(query.getContent())
-    let tokeniser: ILexer;
+  const category = categorise(query.getContent());
+  let tokeniser: ILexer;
 
-    switch (category) {
-        case "select": {
-            tokeniser = new Select();
-        }
-        case "use": {
-            tokeniser = new Use();
-        }
-        default:
-            tokeniser = new Use();
-    }
+  if (category === "select") {
+    tokeniser = new Select();
+  } else if (category === "use") {
+    tokeniser = new Use();
+  } else {
+    tokeniser = new Use();
+  }
 
-    const tokens = tokeniser.tokenise(query)
-    return tokens;
+  const tokens = tokeniser.tokenise(query);
+  return tokens;
 }
 
 export { categorise, tokenise };

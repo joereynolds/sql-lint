2e61563ec452156ffd68980fd2139d68afa3321d
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2019-01-29 12:15:57 +0000

    Add tests for checkerRunner.ts

---
+++ test/unit/checkerRunner.test.ts
@@ -0,0 +1,22 @@
+import { CheckerRunner } from "../../src/checker/checkerRunner";
+import { Printer } from "../../src/printer";
+
+// xtest("It runs database checks when a database is supplied", () => {});
+
+test("It does not run database checks when a database is not supplied", () => {
+  const mockRunDatabaseChecksFn = (CheckerRunner.prototype.runDatabaseChecks = jest.fn());
+  const printer = new Printer(0);
+  const runner = new CheckerRunner();
+  runner.run([], printer, "");
+
+  expect(mockRunDatabaseChecksFn).toHaveBeenCalledTimes(0);
+});
+
+test("It runs basic checks when no database is supplied", () => {
+  const mockRunSimpleChecksFn = (CheckerRunner.prototype.runSimpleChecks = jest.fn());
+  const printer = new Printer(0);
+  const runner = new CheckerRunner();
+  runner.run([], printer, "");
+
+  expect(mockRunSimpleChecksFn).toHaveBeenCalledTimes(1);
+});

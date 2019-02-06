92afc56e8739b0905b7424310b9a64cbdae300de
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2019-02-06 12:10:52 +0000

    Revert "Bump to v0.0.9"
    This reverts commit 2ace6b2f50422dce8bf3778cefa60f34b76a8ff8.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ import { Query } from "./reader/query";
 import { file, getConfiguration } from "./config";
 import { CheckerRunner } from "./checker/checkerRunner";
 
-const version = "0.0.9";
+const version = "0.0.8";
 
 program
   .version(version)

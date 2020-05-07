db6e061c3ad8646e0a361c348546c025e6b94f37
Joe Reynolds <joereynolds952@gmail.com>
2020-05-07 13:29:55 +0100

    Add grammar for DROP DATABASE|SCHEMA

--- src/grammar/drop/grammar.ts
+++ src/grammar/drop/grammar.ts
@@ -14,7 +14,9 @@ var grammar = {
     {"name": "wschar", "symbols": [/[ \t\n\v\f]/], "postprocess": id},
     {"name": "statement", "symbols": ["drop_statements", "_", "terminator"]},
     {"name": "drop_statements", "symbols": ["drop_table"]},
+    {"name": "drop_statements", "symbols": ["drop_database"]},
     {"name": "drop_table", "symbols": ["keyword", "temporary", "table", "clause_if_exists", "drop_table_list", "clause_end_options"]},
+    {"name": "drop_database", "symbols": ["keyword", "clause_database_or_schema", "_", "clause_if_exists", "drop_database_specifier"]},
     {"name": "keyword$subexpression$1", "symbols": [/[dD]/, /[rR]/, /[oO]/, /[pP]/], "postprocess": function(d) {return d.join(""); }},
     {"name": "keyword", "symbols": ["keyword$subexpression$1", "__"], "postprocess": (word) => word.join("")},
     {"name": "temporary", "symbols": []},
@@ -22,6 +24,11 @@ var grammar = {
     {"name": "temporary", "symbols": ["temporary$subexpression$1", "__"]},
     {"name": "table$subexpression$1", "symbols": [/[tT]/, /[aA]/, /[bB]/, /[lL]/, /[eE]/], "postprocess": function(d) {return d.join(""); }},
     {"name": "table", "symbols": ["table$subexpression$1", "__"]},
+    {"name": "clause_database_or_schema", "symbols": []},
+    {"name": "clause_database_or_schema$subexpression$1", "symbols": [/[dD]/, /[aA]/, /[tT]/, /[aA]/, /[bB]/, /[aA]/, /[sS]/, /[eE]/], "postprocess": function(d) {return d.join(""); }},
+    {"name": "clause_database_or_schema", "symbols": ["clause_database_or_schema$subexpression$1", "__"]},
+    {"name": "clause_database_or_schema$subexpression$2", "symbols": [/[sS]/, /[cC]/, /[hH]/, /[eE]/, /[mM]/, /[aA]/], "postprocess": function(d) {return d.join(""); }},
+    {"name": "clause_database_or_schema", "symbols": ["clause_database_or_schema$subexpression$2", "__"]},
     {"name": "clause_if_exists", "symbols": []},
     {"name": "clause_if_exists$subexpression$1", "symbols": [/[iI]/, /[fF]/], "postprocess": function(d) {return d.join(""); }},
     {"name": "clause_if_exists$subexpression$2", "symbols": [/[eE]/, /[xX]/, /[iI]/, /[sS]/, /[tT]/, /[sS]/], "postprocess": function(d) {return d.join(""); }},
@@ -35,6 +42,9 @@ var grammar = {
     {"name": "drop_table_list$ebnf$1", "symbols": ["drop_table_list$ebnf$1", /[a-z]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
     {"name": "drop_table_list", "symbols": ["drop_table_list$ebnf$1"]},
     {"name": "drop_table_list", "symbols": ["drop_table_list", "__", {"literal":","}, "drop_table_list", "_"]},
+    {"name": "drop_database_specifier$ebnf$1", "symbols": [/[a-z]/]},
+    {"name": "drop_database_specifier$ebnf$1", "symbols": ["drop_database_specifier$ebnf$1", /[a-z]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
+    {"name": "drop_database_specifier", "symbols": ["drop_database_specifier$ebnf$1"]},
     {"name": "terminator", "symbols": [{"literal":";"}]}
 ]
   , ParserStart: "statement"
--- src/grammar/drop/statement.ne
+++ src/grammar/drop/statement.ne
@@ -1,18 +1,23 @@
-@preprocessor typescript
+# @preprocessor typescript
 
 @builtin "whitespace.ne" # `_` means arbitrary amount of whitespace
 
 statement -> drop_statements _ terminator
 
-drop_statements -> drop_table 
+drop_statements -> drop_table | drop_database
 
 drop_table -> keyword temporary table clause_if_exists drop_table_list clause_end_options
+drop_database -> keyword clause_database_or_schema _ clause_if_exists drop_database_specifier
 
 keyword -> "DROP"i __  {% (word) => word.join("") %}
 temporary -> null | "TEMPORARY"i __
 table -> "TABLE"i __ 
+
+clause_database_or_schema -> "DATABASE"i __ | "SCHEMA"i __
 clause_if_exists -> null | "IF"i __ "EXISTS"i __
 clause_end_options -> null | __ "RESTRICT"i __ | "CASCADE"i __ 
+
 drop_table_list -> [a-z]:+ | drop_table_list __ "," drop_table_list _
+drop_database_specifier -> [a-z]:+
 
 terminator -> ";"

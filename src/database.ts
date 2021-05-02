3b65b9320ededd6440003baa9ab360bbda5f344a
Lewis Steele <lewis.steele@hey.com>
2021-05-02 17:05:06 +0100

    ⚰️ Remove getDatabases

--- src/database.ts
+++ src/database.ts
@@ -30,16 +30,6 @@ class Database {
     }
   }
 
-  public getDatabases(connection: any, callback: any): void {
-    connection.query("SHOW DATABASES", (error: string, results: string[]) => {
-      if (error) {
-        return console.log(error);
-      }
-
-      callback(results);
-    });
-  }
-
   /**
    * Runs an EXPLAIN on the query. If it doesn't run successfully, errors will come through,
    * which is what we want.

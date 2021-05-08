e7cfee92a6dbdd907878aff1396c24bec6a9c9ed
Lewis Steele <lewis.steele@hey.com>
2021-05-08 15:04:38 +0100

    Add postgres error to check factory

--- src/barrel/checks.ts
+++ src/barrel/checks.ts
@@ -4,15 +4,16 @@ export * from "../checker/checks/any/invalidLimitQuantifier";
 export * from "../checker/checks/any/missingWhere";
 export * from "../checker/checks/any/oddCodePoint";
 export * from "../checker/checks/any/tableNotFound";
-export * from "../checker/checks/any/unmatchedParentheses";
 export * from "../checker/checks/any/trailingWhitespace";
+export * from "../checker/checks/any/unmatchedParentheses";
 
+export * from "../checker/checks/mysql/mySqlError";
 export * from "../checker/checks/mysql/mySqlInvalidAlterOption";
 export * from "../checker/checks/mysql/mySqlInvalidCreateOption";
 export * from "../checker/checks/mysql/mySqlInvalidDropOption";
 export * from "../checker/checks/mysql/mySqlInvalidTruncateOption";
-export * from "../checker/checks/mysql/mySqlError";
 
+export * from "../checker/checks/postgres/postgresError";
 export * from "../checker/checks/postgres/postgresInvalidAlterOption";
 export * from "../checker/checks/postgres/postgresInvalidCreateOption";
 export * from "../checker/checks/postgres/postgresInvalidDropOption";

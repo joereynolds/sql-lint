4dd2281421cc4b299d0f3fc11ae6f8e67de288fa
Lewis Steele <lewis.steele@hey.com>
2021-05-22 18:43:48 +0100

    Add IMessage to getMessage return type

--- src/formatter/interface.ts
+++ src/formatter/interface.ts
@@ -1,7 +1,7 @@
 import { CheckerResult } from "../checker/checkerResult";
 
 interface IFormat {
-  getMessage(prefix: string, result: CheckerResult, verbosity: number): string|any;
+  getMessage(prefix: string, result: CheckerResult, verbosity: number): string|IMessage
 }
 
 interface IMessage {

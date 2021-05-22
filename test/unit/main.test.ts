39d69883d23e47911e861f06f3b247f4b70e77f3
Lewis Steele <lewis.steele@hey.com>
2021-05-22 18:05:29 +0100

    Expect sqlLint to return array of type IMessage

--- test/unit/main.test.ts
+++ test/unit/main.test.ts
@@ -2,7 +2,12 @@ import sqlLint from "../../src/main";
 
 test("it runs the linter programmatically", () => {
   const errors = sqlLint({
-    sql: "DELETE FROM some_table"
+    sql: "DELETE FROM some_table;",
   });
+
   expect(errors).toHaveLength(1);
+  expect(errors[0]).toMatchObject({
+    line: 1,
+    error: '[sql-lint: missing-where] DELETE statement missing WHERE clause.',
+  });
 });

369bb586dd12d3721081ff410f9f3d0feee99fca
Lewis Steele <lewis.steele@hey.com>
2021-05-22 18:07:17 +0100

    Add interface for messages

--- src/formatter/interface.ts
+++ src/formatter/interface.ts
@@ -1,7 +1,14 @@
 import { CheckerResult } from "../checker/checkerResult";
 
 interface IFormat {
-  getMessage(prefix: string, result: CheckerResult, verbosity: number): string;
+  getMessage(prefix: string, result: CheckerResult, verbosity: number): string|any;
 }
 
-export { IFormat };
+interface IMessage {
+  line: number
+  error: string
+  source: string
+  additionalInformation: string
+}
+
+export { IFormat, IMessage };

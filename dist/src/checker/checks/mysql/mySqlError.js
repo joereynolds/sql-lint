4e33bb46129530de1e5663809c4ab4cea594cac4
Lewis Steele <lewis.steele@hey.com>
2021-06-15 19:54:53 +0100

    Delete dist folder

--- dist/src/checker/checks/mysql/mySqlError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MySqlError = void 0;
-const sqlError_1 = require("../generic/sqlError");
-class MySqlError extends sqlError_1.default {
-}
-exports.MySqlError = MySqlError;
-//# sourceMappingURL=mySqlError.js.map

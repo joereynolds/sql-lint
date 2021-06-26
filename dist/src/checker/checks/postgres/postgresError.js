9d78e682c556b0c5d3e6fb773c3a2807052528e1
Lewis Steele <lewis.steele@hey.com>
2021-06-26 11:52:03 +0100

    Remove the dist folder from git (#192)
    * Delete dist folder
    
    * Git ignore dist folder

--- dist/src/checker/checks/postgres/postgresError.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PostgresError = void 0;
-const sqlError_1 = require("../generic/sqlError");
-class PostgresError extends sqlError_1.default {
-}
-exports.PostgresError = PostgresError;
-//# sourceMappingURL=postgresError.js.map

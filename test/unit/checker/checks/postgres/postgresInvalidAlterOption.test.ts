joereynolds/sql-lint

commit 9b51f2286fec84368fd9dd6eca0b4e8295de3743

(#203) Add `SEQUENCE` for postgres (#204)
* Create alter.ts

* Update create.ts

* Update drop.ts

* Update postgresInvalidCreateOption.test.ts

src/lexer/statements/postgres/alter.ts
@@ -15,6 +15,7 @@ class Alter implements ILexer {
     "event",
     "function",
     "procedure",
+    "sequence",
     "server",
     "table",
     "tablespace",

src/lexer/statements/postgres/create.ts
@@ -17,6 +17,7 @@ class PostgresCreate implements ILexer {
     "or",
     "procedure",
     "role",
+    "sequence",
     "server",
     "schema",
     "table",

src/lexer/statements/postgres/drop.ts
@@ -14,6 +14,7 @@ class PostgresDrop implements ILexer {
     "logfile",
     "procedure",
     "schema",
+    "sequence",
     "server",
     "table",
     "view",

test/unit/checker/checks/postgres/postgresInvalidCreateOption.test.ts
@@ -7,6 +7,7 @@ test.each([
   ["CREATE SCHEMA person;"],
   ["CREATE TYPE blah;"],
   ["CREATE EXTENSION blah;"],
+  ["CREATE SEQUENCE blah;"],
 ])("it does not warn about valid CREATE options", (query) => {
   const checker = new PostgresInvalidCreateOption();
 

commit 2221b7eeb6af6a76230838649c5ea7db75aa72c1

fix(postgres): use PostgresAlter instead of Alter (mysql) for postgres
Use `lexer/statements/postgres/alter`
(previously: `Alter`, new: `PostgresAlter`)
instead of `lexer/statements/mysql/alter` (`Alter`).

Add PostgresAlter to exports at statements.

Add test case for postgres-invalid-alter-option.

Without the changes the test failed for option `sequence`
introduced by PR #204.

Relates-to: PR #204

src/barrel/statements.ts
@@ -6,6 +6,7 @@ export * from "../lexer/statements/mysql/truncate";
 export * from "../lexer/statements/mysql/use";
 export * from "../lexer/statements/mysql/rename";
 
+export * from "../lexer/statements/postgres/alter";
 export * from "../lexer/statements/postgres/create";
 export * from "../lexer/statements/postgres/drop";
 export * from "../lexer/statements/postgres/select";

src/checker/checks/postgres/postgresInvalidAlterOption.ts
@@ -1,9 +1,9 @@
 import { IChecker } from "../../interface";
-import { Alter } from "../../../barrel/statements";
+import { PostgresAlter } from "../../../barrel/statements";
 import { InvalidOption } from "../invalidOption";
 
 class PostgresInvalidAlterOption extends InvalidOption implements IChecker {
-  public checker = new Alter();
+  public checker = new PostgresAlter();
   public appliesTo = ["alter"];
 }
 

src/lexer/statements/postgres/alter.ts
@@ -5,7 +5,7 @@ import { Types } from "../../types";
 import { Keyword } from "../../../syntax/keywords";
 import { Token } from "../../token";
 
-class Alter implements ILexer {
+class PostgresAlter implements ILexer {
   public options: string[] = [
     "column",
     "online",
@@ -47,4 +47,4 @@ class Alter implements ILexer {
   }
 }
 
-export { Alter };
+export { PostgresAlter };

test/unit/checker/checks/postgres/postgresInvalidAlterOption.test.ts
@@ -1,0 +1,27 @@
+import { PostgresInvalidAlterOption } from "../../../../../src/checker/checks/postgres/postgresInvalidAlterOption";
+import { tokenise } from "../../../../../src/lexer/lexer";
+import { putContentIntoLines } from "../../../../../src/reader/reader";
+
+test.each([
+  ["ALTER column;"],
+  ["ALTER online;"],
+  ["ALTER offline;"],
+  ["ALTER ignore;"],
+  ["ALTER database;"],
+  ["ALTER event;"],
+  ["ALTER function;"],
+  ["ALTER procedure;"],
+  ["ALTER sequence;"],
+  ["ALTER server;"],
+  ["ALTER table;"],
+  ["ALTER tablespace;"],
+  ["ALTER view;"],
+])("it does not error about valid ALTER options", (query) => {
+  const checker = new PostgresInvalidAlterOption();
+
+  const queryObj = putContentIntoLines(query);
+  const tokenised = tokenise(queryObj[0]);
+
+  const actual = checker.check(tokenised);
+  expect(actual.content).toBeFalsy();
+});
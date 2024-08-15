bcbefc4a85dfb5c65e541e5fae6f11d98c6f8f1e
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2024-08-15 11:09:07 +0100

    Add support for postgres PUBLICATION

--- src/lexer/statements/postgres/create.ts
+++ src/lexer/statements/postgres/create.ts
@@ -17,6 +17,7 @@ class PostgresCreate implements ILexer {
     "materialized",
     "or",
     "procedure",
+    "publication",
     "role",
     "sequence",
     "server",
--- test/unit/checker/checks/postgres/postgresInvalidCreateOption.test.ts
+++ test/unit/checker/checks/postgres/postgresInvalidCreateOption.test.ts
@@ -9,6 +9,7 @@ test.each([
   ["CREATE MATERIALIZED VIEW blah;"],
   ["CREATE VIEW blah;"],
   ["CREATE EXTENSION blah;"],
+  ["CREATE PUBLICATION blah;"],
   ["CREATE SEQUENCE blah;"],
 ])("it does not warn about valid CREATE options", (query) => {
   const checker = new PostgresInvalidCreateOption();
